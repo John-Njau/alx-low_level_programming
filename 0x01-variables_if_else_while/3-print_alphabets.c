4e5c654c1673d8f6a194e8205e200ce70ce73e6c
John-Njau <johnnjaunjoroge@gmail.com>
2022-09-11 23:23:51 +0300

    Alphabets in lowercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
- * Main : Entry point
- * Fn: prints the alphabet in lowercase, and then in uppercase
- * returns success
+ * main - Entry point
+ *
+ * Return: Alphabets in lowercase then in uppercase
  */
+
 int main(void)
 {
-    char i;
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        putchar(i);
-    }
-    for (i = 'A'; i <= 'Z'; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (i = 'A'; i <= 'Z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
