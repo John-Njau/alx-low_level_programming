f467157b381fa4af2fb8ab722cfc372756388f91
John-Njau <johnnjaunjoroge@gmail.com>
2022-09-12 14:01:24 +0300

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all base 16 numbers in lowercase
+ *
+ * Return: Success always 0.
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar((i % 10) + '0');
+putchar((i  %10, ','));
+putchar((i  %10, ' '));
+}
+putchar('\n');
+return (0);
+}
