87b8231657088dec4b3c5a224f23d04aebf8eb4c
John Njau <johnnjaunjoroge@gmail.com>
2022-09-15 13:27:26 +0300

    task 3 - is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-int r;
-
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
-}
-
-/**
  * _islower - returns 0 if uppercase and 1 if otherwise
  *
  * @c: int value
