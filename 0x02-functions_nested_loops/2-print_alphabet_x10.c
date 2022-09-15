8634ccd16b1a19326cfc1da2b1510ece3af96fe1
John Njau <johnnjaunjoroge@gmail.com>
2022-09-15 14:21:40 +0300

    is alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,6 @@
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
 
-int main(void)
-{
-    print_alphabet_x10();
-    return (0);
-}
-
 void print_alphabet_x10(void)
 {
 int count = 0;
