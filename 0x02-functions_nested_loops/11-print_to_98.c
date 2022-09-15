4513081e522c3d5b821138dc6d0072f94baf3fd4
John Njau <johnnjaunjoroge@gmail.com>
2022-09-15 17:07:47 +0300

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+print_to_98(0);
+print_to_98(98);
+print_to_98(111);
+print_to_98(81);
+print_to_98(-10);
+return (0);
+}
+
+voidprint_to_98(int n)
+{
+
+if (n >= 98)
+{
+while (n > 98)
+{
+printf("%d,", n--);
+}
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+{
+printf("%d,", n++);
+}
+printf("%d\n", n);
+}
+}
