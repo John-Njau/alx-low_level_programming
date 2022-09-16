b3f6befb306d9c44707303acd89e47f060505b1c
John Njau <johnnjaunjoroge@gmail.com>
2022-09-16 22:22:49 +0300

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplication
+ *
+ * @a: multiplication param
+ * @b:  multiplication param
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+
+}
