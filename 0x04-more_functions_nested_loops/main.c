0c328705a633d24fddef9414adcdd30334d54509
John Njau <johnnjaunjoroge@gmail.com>
2022-09-19 09:43:56 +0300

    print triangle

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main - check the code
  *
@@ -7,8 +8,9 @@
  */
 int main(void)
 {
-    print_square(2);
-    print_square(10);
-    print_square(0);
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
     return (0);
 }
