cb87cda23a0b894eca5cbd5c22c29905eb6e10c1
John Njau <johnnjaunjoroge@gmail.com>
2022-09-20 13:59:00 +0300

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,12 @@
 #include "main.h"
+#include <string.h>
 
+size_t _strlen(const char *s)
+{
+size_t length = 0;
+
+while (*s++)
+length ++;
+
+return length;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 /*
 * File: main.h
 * Header file that defines all declarations
@@ -9,5 +11,6 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+size_t _strlen(const char *s);
 
 #endif
