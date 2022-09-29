5d40b94f5eaad55977a434f1ec02266976ab0b16
John Njau <johnnjaunjoroge@gmail.com>
2022-09-29 13:44:42 +0300

    strspn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void *_memset(void *s, int c, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
