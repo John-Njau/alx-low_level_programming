fe3151005b6d5986c8129f10b479b4913c0ad38b
John Njau <johnnjaunjoroge@gmail.com>
2022-10-03 22:06:55 +0300

    qrgs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -- Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return:int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+}
