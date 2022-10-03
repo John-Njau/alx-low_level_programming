a1854005e30e28a7f1a14c4d4c9876f64f3480e2
John Njau <johnnjaunjoroge@gmail.com>
2022-10-03 22:01:08 +0300

    what is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,10 @@
  *
  * @argc: Argument count
  * @argv: Argument vector
- * Return: int
+ * Return: int Always 0
  */
 
-int main(int argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char **argv)
 {
 		printf("%s\n", argv[0]);
 }
